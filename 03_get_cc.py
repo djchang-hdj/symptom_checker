101f8f5c3f1b550ee5b8b424c9f65fa29bd3f7c9
ChangDJ <hpalways@DJsMBP14.local>
2024-06-18 09:30:58 +0900

    correct cc type from string to list

--- 03_get_cc.py
+++ 03_get_cc.py
@@ -10,64 +10,61 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def make_cc_of_oneself(client, symptom):
     response = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {
-        "role": "system",
-        f"content": f"""You are a doctor studying diagnostics. 
-        You are simulating a patient with a certain symptom. 
-        Patients with this symptom may present to the doctor in a variety of ways. 
-        Create three chief complaints for the symptom in Korean.
-        Keep it simple and concise. and give a list as a response.
-        ["CC1", "CC2", "CC3"]
-        """
-        },
-        {
-        "role": "user",
-        "content": f"{symptom}"
-        }
-    ],
-    temperature=0.8,
-    top_p=1
-)   
-    return response.choices[0].message.content
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": f"""You are a doctor studying diagnostics. 
+                You are simulating a patient with a certain symptom. 
+                Patients with this symptom may present to the doctor in a variety of ways. 
+                Create three chief complaints for the symptom in Korean.
+                Keep it simple and concise. and give a list as a response.
+                ["CC1", "CC2", "CC3"]
+                """
+            },
+            {
+                "role": "user",
+                "content": f"{symptom}"
+            }
+        ],
+        temperature=0.8,
+        top_p=1
+    )   
+    return json.loads(response.choices[0].message.content)
 
 
 def make_cc_for_ones_child(client, symptom):
     response = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {
-        "role": "system",
-        f"content": f"""You are a doctor studying diagnostics. 
-        You are simulating a parent who has a child with a certain symptom. 
-        The child with this symptom may present to the doctor in a variety of ways. 
-        Create three chief complaints for the symptom in Korean.
-        Keep it simple and concise. and give a list as a response.
-        ["아이가 __ ", "아이가 __", "아이가 __ "]
-        """
-        },
-        {
-        "role": "user",
-        "content": f"{symptom}"
-        }
-    ],
-    temperature=0.8,
-    top_p=1
-)   
-    return response.choices[0].message.content
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": f"""You are a doctor studying diagnostics. 
+                You are simulating a parent who has a child with a certain symptom. 
+                The child with this symptom may present to the doctor in a variety of ways. 
+                Create three chief complaints for the symptom in Korean.
+                Keep it simple and concise. and give a list as a response.
+                ["아이가 __ ", "아이가 __", "아이가 __ "]
+                """
+            },
+            {
+                "role": "user",
+                "content": f"{symptom}"
+            }
+        ],
+        temperature=0.8,
+        top_p=1
+    )   
+    return json.loads(response.choices[0].message.content)
 
-
-# 증상에 adults 문구가 있으면 make_cc_of_oneself 호출, 증상에 children 문구가 있으면 make_cc_for_ones_child 호출 함수
 
 def get_cc(client, symptom):
-    if "adults" in symptom:
+    if "adult" in symptom:
         return make_cc_of_oneself(client, symptom)
     elif "children" in symptom:
         return make_cc_for_ones_child(client, symptom)
     else:
         return None
-    
 
 
 def read_json_file(file_path):
@@ -76,9 +73,6 @@ def read_json_file(file_path):
     
 symptoms = read_json_file("links_dict.json").keys()
 
-#save to json file
-# key : symptom, value : cc_list
-# Use tqdm to show progress
 output = {symptom: get_cc(client, symptom) for symptom in tqdm(symptoms)}
 
 with open('cc_list.json', 'w', encoding='utf-8') as file:
